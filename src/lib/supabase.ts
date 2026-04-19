7dbffacd1948e6ada958946519174d32532fbd2f
Jairo Abreu Filho <1403787+jairoabreu@users.noreply.github.com>
2026-04-19 19:52:12 -0300

    fix: add ts-ignore for import.meta.env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,9 @@
 // src/lib/supabase.ts
 import { createClient } from '@supabase/supabase-js'
 
+// @ts-ignore
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string
+// @ts-ignore
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY as string
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
